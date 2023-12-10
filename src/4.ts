5703cb11fc2dbd6bdd505b734bb83dc8d3d8fceb
Liasique <olgalukianets7@gmail.com>
2023-12-10 17:29:14 +0100

    update

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,64 @@
-const key = new Key();
-
-const house = new MyHouse(key);
-const person = new Person(key);
-
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
-
-export {};
+class Key {
+    private signature: string;
+  
+    constructor() {
+      this.signature = Math.random().toString();
+    }
+  
+    getSignature(): string {
+      return this.signature;
+    }
+  }
+  
+  class Person {
+    private key: Key;
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
+  abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    abstract openDoor(key: Key): void;
+  
+    comeIn(person: Person): void {
+      if (this.door) {
+        this.tenants.push(person);
+        console.log('Person has entered the house');
+      } else {
+        console.log('Door is closed. Cannot enter.');
+      }
+    }
+  }
+  
+  class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+        console.log('Door opened successfully');
+      } else {
+        console.log('Wrong key. Door remains closed.');
+      }
+    }
+  }
+  
+  // Тестування сценарію
+  const key = new Key();
+  const house = new MyHouse(key);
+  const person = new Person(key);
+  
+  house.openDoor(person.getKey());
+  house.comeIn(person);
+  
